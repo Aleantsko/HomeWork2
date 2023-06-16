7c17d7fc58f504aa86c6ce4f63997a93edcd62f1
Aleantsko <135431196+Aleantsko@users.noreply.github.com>
2023-06-16 18:17:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 1
+// Console.Clear();
+// Console.WriteLine("Введите число а: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine((a % 100) / 10);
+
+
+
+//Задача 2
+// Console.Clear();
+// Console.WriteLine("Введите число а: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int tens = 1;
+// if (a < 100)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//     while (tens * 10 < a)
+//     {
+//         tens *= 10;
+//     }
+//     Console.WriteLine(tens);
+//     Console.WriteLine((a / ((float)tens / 100)) % 10);
+// }
+
+
+
+//Задача 3
+// Console.Clear();
+// Console.WriteLine("Введите число а: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a >= 6 && a <= 7)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
